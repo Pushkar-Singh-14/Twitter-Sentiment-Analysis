5056e2f0e9d927dc38895f678d2acf102a54098f
Pushkar-Singh-14 <46190572+Pushkar-Singh-14@users.noreply.github.com>
2019-02-22 22:29:44 +0530

    Update Live Graph.py

--- Live Graph.py
+++ Live Graph.py
@@ -7,9 +7,7 @@ style.use("ggplot")
 
 fig = plt.figure()
 ax1 = fig.add_subplot(2,2,1)
-ax2 = fig.add_subplot(2,2,2)
-ax3 = fig.add_subplot(2,2,3)
-ax4 = fig.add_subplot(2,2,4)
+
 
 def animate(i):
     pullData = open("/Users/pushkarsingh/Desktop/twitter/test-yy_twitter.txt","r").read()
@@ -31,13 +29,7 @@ def animate(i):
         yar.append(y)
         
     ax1.clear()
-    ax2.clear()
-    ax3.clear()
-    ax4.clear()
-    
     ax1.plot(xar,yar)
-    ax2.plot(xar,yar)
-    ax3.plot(xar,yar)
-    ax4.plot(xar,yar)
+    
 ani = animation.FuncAnimation(fig, animate, interval=1000,save_count=1000)
 plt.show()
